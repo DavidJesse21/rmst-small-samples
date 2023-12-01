d83ce8a8f4a0a040b101b865dae292ad1fdcf292
David Jesse <david.jesse@stud.uni-goettingen.de>
2023-12-01 10:45:23 +0100

    Continue investigating scenarios, in which RMST should not be estimable

--- playground/rmst_err.R
+++ playground/rmst_err.R
@@ -2,16 +2,24 @@ options(box.path = "R")
 
 box::use(
   data.table[...],
-  survival[Surv]
+  survival[Surv, survfit]
 )
 
 box::use(
   rmst/km[rmst]
 )
 
+rmst_surv = function(formula, data, cutoff) {
+  fit = survfit(formula, data)
+  smry = summary(fit, rmean = cutoff)
+  smry$table[5:6]
+}
+
+
+# Example data ----
+
 dt = survival::veteran
 setDT(dt)
-
 for (j in setdiff(colnames(dt), c("trt", "time", "status"))) {
   set(dt, j = j, value = NULL)
 }
@@ -19,34 +27,42 @@ for (j in setdiff(colnames(dt), c("trt", "time", "status"))) {
 dt = dt[order(time)]
 # remove 2 largest observations
 dt = dt[time < 900]
-
-# This should work with no problems
-# rmst(Surv(time, status) ~ 1, data = dt, cutoff = 400)
-
+# Now the largest event time is 600
 dt[, max(time)]
 
-# This should work although cutoff is larger than largest event time as long as 
-# we have observed the last event time.
+
+# Last event time observed ----
+
+# As long as the last event has been observed computing the RMST with a cutoff (restriction time) 
+# that is greater than the last event time should be no problem
+
+# Own function
 rmst(Surv(time, status) ~ 1, data = dt, cutoff = 600)
+# survRM2
+with(dt, survRM2:::rmst1(time, status, 600))$rmst
+# survival
+rmst_surv(Surv(time, status) ~ 1, dt, cutoff = 600)
 
 
+# Last event time censored ----
 
-# assume last observation is censored
-# this should actually not work
+# Assume last observation is censored
 dt[.N, status := 0L]
-rmst(Surv(time, status) ~ 1, data = dt, cutoff = 600)
-# but it does work
 
-# How does rmst2 handle this?
-with(
-  dt,
-  survRM2:::rmst1(time, status, 600)
-)
-# returns the same estimates
-with(
-  dt,
-  survRM2perm:::rmst1_update(time, status, 600)
-)
+# The RMST functions should now actually not work or at least produce a warning, since 
+# the RMST is not uniquely defined beyond the latest event time, which is now censored.
+
+# Own function
+rmst(Surv(time, status) ~ 1, data = dt, cutoff = 600)
+# survRM2
+with(dt, survRM2:::rmst1(time, status, 600))$rmst
+# survival
+rmst_surv(Surv(time, status) ~ 1, dt, cutoff = 600)
+
+# All three functions still work like before and produce the same results.
+
+
+# eventglm (pseudo-observations) ----
 
 box::use(
   eventglm[rmeanglm]
@@ -62,6 +78,24 @@ m = rmeanglm(
   Surv(time, status) ~ factor(trt), data = dt, time = 600,
   model.censoring = "stratified", formula.censoring = ~ factor(trt)
 )
+
+# !!!
+m = rmeanglm(
+  Surv(time, status) ~ factor(trt), data = dt, time = 553,
+  model.censoring = "stratified", formula.censoring = ~ factor(trt)
+)
+m = rmeanglm(
+  Surv(time, status) ~ factor(trt), data = dt, time = 554,
+  model.censoring = "stratified", formula.censoring = ~ factor(trt)
+)
+m = rmeanglm(
+  Surv(time, status) ~ factor(trt), data = dt, time = 554#,
+  #model.censoring = "stratified", formula.censoring = ~ factor(trt)
+)
+
+
+
+summary(m)
+
 traceback()
 
-survRM2perm:::rmst1_update()
